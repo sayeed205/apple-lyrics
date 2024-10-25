4893ca70bacf5fdca572ee06c6bfb0b3e8629972
Hitarashi <sayeed205@gmail.com>
2024-10-25 15:56:09 +0530

    add searching lyrics

---
+++ src/app/api/route.ts
@@ -0,0 +1,100 @@
+import {
+  getToken,
+  getTTML,
+  isValidAppleMusicUrl,
+  parseAppleMusicURL,
+} from "@/lib/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const urlParam = request.nextUrl.searchParams.get("url");
+  const idParam = request.nextUrl.searchParams.get("id");
+
+  let id = "";
+  let store = "in"; // Default to 'us' if store is not provided
+
+  if (urlParam) {
+    // Validate and parse Apple Music URL
+    if (isValidAppleMusicUrl(urlParam)) {
+      const meta = parseAppleMusicURL(urlParam);
+      console.log("meta", meta);
+      if (meta) {
+        id = meta.id;
+        store = meta.store;
+      } else {
+        return NextResponse.json(
+          {
+            success: false,
+            message: "Invalid Apple Music URL",
+            status: 400,
+            data: null,
+          },
+          { status: 400 },
+        );
+      }
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid URL format",
+          status: 400,
+          data: null,
+        },
+        { status: 400 },
+      );
+    }
+  } else if (idParam) {
+    // Validate id parameter
+    try {
+      id = parseInt(idParam).toString();
+    } catch (error) {
+      console.error("Failed to parse Apple Music id: ", error);
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid Apple Music id",
+          status: 400,
+          data: null,
+        },
+        { status: 400 },
+      );
+    }
+  } else {
+    // No id or url parameter provided
+    return NextResponse.json(
+      {
+        success: false,
+        message: "No id or url provided",
+        status: 400,
+        data: null,
+      },
+      { status: 400 },
+    );
+  }
+
+  // Fetching lyrics
+  try {
+    const token = await getToken();
+    const ttml = await getTTML(id, token, store);
+    return NextResponse.json(
+      {
+        success: true,
+        message: ttml ? "Lyrics Found" : "Lyrics not found",
+        status: ttml ? 200 : 404,
+        data: ttml,
+      },
+      { status: ttml ? 200 : 404 },
+    );
+  } catch (err) {
+    console.error("Failed to get Lyrics: ", err);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error fetching lyrics",
+        status: 400,
+        data: null,
+      },
+      { status: 400 },
+    );
+  }
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,97 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+const BASE_URL = "https://beta.music.apple.com";
+
+export const getToken = async (): Promise<string> => {
+  try {
+    const response = await fetch(BASE_URL);
+    const body = await response.text();
+
+    const regex = /\/assets\/index-legacy-[^/]+\.js/;
+    const indexJsUri = body.match(regex)?.[0];
+
+    if (!indexJsUri) {
+      throw new Error("Index JS URI not found");
+    }
+
+    const indexResponse = await fetch(BASE_URL + indexJsUri);
+    const indexBody = await indexResponse.text();
+
+    const tokenRegex = /eyJh([^"]*)/;
+    const token = indexBody.match(tokenRegex)?.[0];
+
+    if (!token) {
+      throw new Error("Token not found");
+    }
+
+    return token;
+  } catch (error) {
+    console.error("Failed to get token:", error);
+    throw new Error(
+      "Failed to get token and no valid authorization token found.",
+    );
+  }
+};
+
+export const getTTML = async (
+  trackId: string,
+  token: string,
+  storefront = "in",
+) => {
+  const url = `https://amp-api.music.apple.com/v1/catalog/${storefront}/songs/${trackId}/syllable-lyrics`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Origin: "https://music.apple.com",
+        Referer: "https://music.apple.com/",
+        Authorization: `Bearer ${token}`,
+        Cookie: `media-user-token=${process.env.MEDIA_USER_TOKEN}`,
+      },
+    });
+
+    if (!response.ok) {
+      return null;
+    }
+
+    const data = await response.json();
+
+    if (data?.data?.[0]?.attributes?.ttml) {
+      return data.data[0].attributes.ttml as string;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Failed to get tTML for track:", error);
+    return null;
+  }
+};
+
+export const parseAppleMusicURL = (url: string) => {
+  // Match `store` and `id` from the path, while allowing for an additional `id` in the query parameter
+  const pathMatch = url.match(
+    /https:\/\/music\.apple\.com\/(in|us)\/\w+\/[\w-]+\/(\d+)/,
+  );
+  const queryMatch = url.match(/[?&]i=(\d+)/);
+
+  if (pathMatch) {
+    return {
+      store: pathMatch[1],
+      // Use `id` from the query parameter if it exists, otherwise use the `id` from the path
+      id: queryMatch ? queryMatch[1] : pathMatch[2],
+    };
+  }
+  return null;
+};
+
+export const isValidAppleMusicUrl = (url: string): boolean => {
+  const pattern =
+    /^https:\/\/music\.apple\.com\/(\w{2})\/(song|album)\/[^/]+\/(\d+)(\?.*i=\d+.*)?$/;
+  return pattern.test(url);
+};
